9991fb383664dbbbf5b231798fdcd6722cc57533
Igor de Medeiros <demedeiros.igor@gmail.com>
2020-05-14 22:41:35 -0300

    Primeiros passos no javascript, arquivo main.js com exemplos

---
+++ js/main.js
@@ -0,0 +1,61 @@
+/*
+var d = new Date();
+alert(d);
+alert(d.getMonth()+1);//o mes sempre começa em 0 por isso para ser o mes certo tem que colocar +1
+alert(d.getMinutes());
+*/
+/*
+var count;
+for(count=0; count<=5; count++){
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    count++;
+};
+*/
+/*
+var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+/*
+var frutas = [{nome:"banana", cor:"amarelo"},{nome:"maça", cor:"vermelhor"}]
+alert(frutas[1].nome);
+console.log(frutas);
+*/
+/*
+var fruta = {nome:"maça", cor:"vermelhor"};
+console.log(fruta);
+console.log(fruta.nome);
+*/
+/*
+var lista = ["maça", "pera", "laranja"];
+console.log(lista);
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+/*
+var nome = "Igor de Medeiros";
+var idade = 37;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos.");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão", "Brasil"));
+*/
